81894804e4e5963b8c768ff4c26afba070c1e088
Kyaw <mail@kyswtn.com>
2024-12-07 10:57:34 +0700

    feat: add r3f-perf

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import {Canvas} from '@react-three/fiber'
+import {Perf} from 'r3f-perf'
 
 const DEV = import.meta.env.DEV
 export default function App() {
   return (
     <Canvas camera={{position: [0, 15, 5]}}>
-      {DEV && <gridHelper />}
+      {DEV && (
+        <>
+          <gridHelper />
+          <Perf position="bottom-left" />
+        </>
+      )}
 
       <mesh>
         <boxGeometry />
